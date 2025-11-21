84ee24e17ee5c8937d8132ad6f64e9cae4b3da87
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-11-22 02:06:44 +0530

    remove Bun example, add Express server and database connection
    Overall folder structure setup is done

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
-console.log("Starting the development school erp backend")
-// Designing the database
+import express from "express"
+import { db } from "./db";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    db
+})
+
+// Middleware to parse JSON requests
+app.use(express.json());
